df3c02486033d105e6988407f0ec8807551ebebc
LinPu817 <2490930309@qq.com>
2022-02-23 18:34:48 +0800

    2022-2-23

---
+++ 分支与循环/while for.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+//{
+//	int a = strlen("");
+//	int b = sizeof("");
+//	printf("%d\n", a);
+//	printf("%d\n", b);
+//	return 0;
+//}
+//int main()
+//{
+//	int ch = 0;
+//	int ret = 0;
+//	char password[30] = { 0 };
+//	printf("请输入密码:");
+//	scanf("%d", &password);
+//	printf("qingshuru Y/N");
+//	while ((ch = getchar()) != '\0')
+//	{
+//		;
+//	}
+//	ret = getchar();
+//	if (ret == 'y')
+//	{
+//		printf("确认成功");
+//	}
+//	else
+//	{
+//		printf("确认失败");
+//	}
+//}
+//int main()
+//{
+//	int a = 0;
+//	for (int i = 0; i < 10; i++)
+//	{
+//		if (i == 5)
+//			break;
+//		printf("%d", i);
+//		//a = a + i;
+//	}
+//	//printf("%d", a);
+//}
